95d8589029d00a320e46b8c27e3740fe9eab0d6e
Nivedh-exp <nivedh@experience.com>
2023-08-25 10:58:44 +0530

    dd

---
+++ Html and Css/Js-sec1.js
@@ -0,0 +1,15 @@
+var a="the java the script"
+console.log(a.split(" "))
+var b="   the work is done     "
+console.log(b.toUpperCase())
+var c="javascript is case sensitive"
+console.log(c.charAt(7))
+console.log(c.substring(5,10))
+console.log(c.indexOf("is"))
+console.log(c.slice(4,10))
+console.log(c.substring(1))
+console.log(a+" "+b+" "+c)
+console.log(b.trim())
+console.log(a.lastIndexOf("the"))
+console.log(c.length);
+console.log(b.includes("i"))
